e27219a320d3b56dc46993d029f826af81862c22
Diogo Dourado <dcdourado@gmail.com>
2022-08-22 20:09:46 -0300

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EctoBackfiller.MixProject do
   def project do
     [
       app: :ecto_backfiller,
-      version: "0.1.0-dev",
+      version: "0.2.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
